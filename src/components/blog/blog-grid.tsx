2dab3711916f7f08b7e34a9decfe6c9ac02c9188
Artrix9095 <sefu786@outlook.com>
2025-02-15 22:13:01 -0800

    Remove blog duplication

--- src/components/blog/blog-grid.tsx
+++ src/components/blog/blog-grid.tsx
@@ -3,12 +3,7 @@ import { GlowArea } from "../ui/glow";
 import BlogCard from "./blog-card";
 import type { BlogPost } from "@/lib/blog";
 
-export default function BlogGrid({ posts: _posts }: { posts: BlogPost[] }) {
-   // Duplicate posts 30 times
-   let posts = Array.from({ length: 30 })
-      .map((_) => _posts)
-      .flat();
-
+export default function BlogGrid({ posts }: { posts: BlogPost[] }) {
    return (
       <GlowArea className="grid items-center justify-center grid-cols-[repeat(auto-fit,minmax(min(428px,100%),600px))] gap-6">
          {posts.map((post, i) => (
